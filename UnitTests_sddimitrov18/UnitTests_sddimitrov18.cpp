5e9d67af5462fe843103cdc609eff204a0c6a0c3
SDDimitrov18 <sddimitrov18@codingburgas.bg>
2021-03-10 09:59:34 +0200

    Done all the test methods and other things

--- UnitTests_sddimitrov18/UnitTests_sddimitrov18.cpp
+++ UnitTests_sddimitrov18/UnitTests_sddimitrov18.cpp
@@ -9,18 +9,53 @@ namespace UnitTestssddimitrov18
 	TEST_CLASS(UnitTestssddimitrov18)
 	{
 	public:
-		
-		TEST_METHOD(TestMethod1)
+		// (10; 20]
+		// 14 => true
+		// 8  => false
+		// 10 => false
+		// 20 => true
+		// 21 => false
+		TEST_METHOD(ShouldReturnTrueIfArgumentIsInRange)
 		{
-			// (10; 20]
-			// 14 => true
-			// 8  => false
-			// 10 => false
-			// 20 => true
-			// 21 => false
+			// AAA
+			
+			//Arrange
 			bool result;
+			int argument = 17;
 
+			//Act
 			result = isNumberInRange(17);
+
+			//Assert
+			Assert::AreEqual(true, result);
+		}
+		TEST_METHOD(ShouldReturnFalseIfArgumentIsOutOfRange)
+		{
+			bool result;
+			int arg = 8;
+			result = isNumberInRange(arg);
+			Assert::AreEqual(false, result);
+		}
+		TEST_METHOD(ShouldReturnFalseIfArgumentIsOutOfRange)
+		{
+			bool result;
+			int arg = 10;
+			result = isNumberInRange(arg);
+			Assert::AreEqual(false, result);
+		}
+		TEST_METHOD(ShouldReturnTrueIfArgumentIsInRange)
+		{
+			bool result;
+			int arg = 20;
+			result = isNumberInRange(arg);
+			Assert::AreEqual(true, result);
+		}
+		TEST_METHOD(ShouldReturnFalseIfArgumentIsOutOfRange)
+		{
+			bool result;
+			int arg = 21;
+			result = isNumberInRange(arg);
+			Assert::AreEqual(false, result);
 		}
 	};
 }
